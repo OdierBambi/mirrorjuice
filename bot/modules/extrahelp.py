91f67c9d14e4d1960839856050a048f3f8a37386
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-10-30 19:08:23 +0700

    Update extrahelp.py

--- bot/modules/extrahelp.py
+++ bot/modules/extrahelp.py
@@ -8,20 +8,17 @@ from bot import dispatcher
 
 def extrahelp(update, context):
     help_string = '''
-   ✙ 🧨 *Menu Xtra* 💠 *:*
- ➻ /song: Get song from youtube
- ➻ /telegraph: Upload file to telegraph
- ➻ /tgt: Upload text to telegraph
- ➻ /whois: get info from user
- ➻ /webss: Upload screenshot from web
- ➻ /tts: Convert text to voice
- ➻ /tl: Use /tl LANGUAGE_CODE
- ➻ /paste: Paste text
- ➻ /ban: Ban user in group
- ➻ /jav: Get jav idol image
- ➻ /cat: Get cat image
+  ✙ *𝐄𝐱𝐭𝐫𝐚* ☠️ *:*
+ ➻ `/song`*:* Get song from youtube
+ ➻ `/tgm`*:* Upload file to telegraph
+ ➻ `/tgt`*:* Upload text to telegraph
+ ➻ `/whois`*:* get info from user
+ ➻ `/webss`*:* Upload screenshot from web
+ ➻ `/tts`*:* Convert text to voice
+ ➻ `/tl`*:* Use /tl [LANGUAGE_CODE]
+ ➻ `/paste`*:* Paste text to pasty
  '''
-    update.effective_message.reply_photo("https://telegra.ph/file/1d77962382170772a14d1.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_photo("https://telegra.ph/file/6b6d2675626aa90f67bce.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
 
 
 EXTRAHELP_HANDLER = CommandHandler("extrahelp", extrahelp)
