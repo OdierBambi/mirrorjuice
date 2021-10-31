1075658390618485c42abf92e3fabd47c3ac09c9
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-11-01 01:31:22 +0700

    Add files via upload

---
+++ bot/modules/welcome.py
@@ -0,0 +1,20 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message, User, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+
+@app.on_message(filters.new_chat_members)
+async def welcome(bot,message):
+	chatid = message.chat.id
+	markup = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⚙ Contact me ⚙', url='https://telegram.me/OdierBambi')
+        ]]
+    )
+	await bot.send_message(text=f"Selamat Datang {message.from_user.mention} to {message.chat.username} ,  Happy to have here", chat_id=chatid, reply_markup=markup)
+	
+@app.on_message(filters.left_chat_member)
+async def goodbye(bot,message):
+	chatid= message.chat.id
+	await bot.send_message(text=f"Selamat jalan ,  {message.from_user.mention} , Semoga harimu menyenangkan😁",chat_id=chatid)
