0661d41ffc6f2011ebc0727a92056e072ca47c70
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-10-31 18:44:57 +0700

    Update ginfo.py

--- bot/modules/ginfo.py
+++ bot/modules/ginfo.py
@@ -12,13 +12,13 @@ def ginfo(client, message):
     txt=message.text
     text = txt.split(" ", 1)
     chat_id=text[1]
-    message = f"<b>Chat Name:</b> {bot.get_chat(chat_id)}"
-    message += f"<b>Total Members:</b> {bot.get_chat_members_count(chat_id)}"
-    message += f"<b>Photo:</b> {bot.get_profile_photos(chat_id, limit=1)}"
-    message += f"<b>Link:</b> {bot.get_chat_invite_link(chat_id)}"
+    message = f"<b>Chat Name:</b> {app.get_chat(chat_id)}"
+    message += f"<b>Total Members:</b> {app.get_chat_members_count(chat_id)}"
+    message += f"<b>Photo:</b> {app.get_profile_photos(chat_id, limit=1)}"
+    message += f"<b>Link:</b> {app.get_chat_invite_link(chat_id)}"
     try:
-        message += f"<b>Pinned Message:</b> {bot.get_dialogs(chat_id, pinned_only=True)}"
-        message += f"<b>Chat Admins:</b> {bot.get_chat_members(chat_id, filter="administrators")}"
+        message += f"<b>Pinned Message:</b> {app.get_dialogs(chat_id, pinned_only=True)}"
+        message += f"<b>Chat Admins:</b> {app.get_chat_members(chat_id, filter="administrators")}"
     except TelegramError as e:
         return 
     message.reply_text(message)
