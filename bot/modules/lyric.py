2f72f58ac0a163d8db736c3f030fe38704b895f5
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-11-01 02:24:44 +0700

    Update lyric.py

--- bot/modules/lyric.py
+++ bot/modules/lyric.py
@@ -1,41 +1,36 @@
-from tswift import Song
-
-from telegram import Bot, Update, Message, Chat
-from telegram.ext import CommandHandler, run_async
-
-from bot import dispatcher
+from tswift import Song
+from telegram import Bot, Update, Message, Chat
+from telegram.ext import CommandHandler
+from bot import dispatcher
 
-
-
-@run_async
-def lyrics(bot: Bot, update: Update, args):
-    msg = update.effective_message
-    query = " ".join(args)
-    song = ""
-    if not query:
-        msg.reply_text("You haven't specified which song to look for!")
-        return
-    else:
-        song = Song.find_song(query)
-        if song:
-            if song.lyrics:
-                reply = song.format()
-            else:
-                reply = "Couldn't find any lyrics for that song!"
-        else:
-            reply = "Song not found!"
-        if len(reply) > 4090:
-            with open("lyrics.txt", 'w') as f:
-                f.write(f"{reply}\n\n\nOwO UwU OmO")
-            with open("lyrics.txt", 'rb') as f:
-                msg.reply_document(document=f,
-                caption="Message length exceeded max limit! Sending as a text file.")
-        else:
-            msg.reply_text(reply)
-                
-        
-
-
-LYRICS_HANDLER = CommandHandler("lyrics", lyrics, pass_args=True)
-
+
+
+def lyrics(bot: Bot, update: Update, args):
+    msg = update.effective_message
+    query = " ".join(args)
+    song = ""
+    if not query:
+        msg.reply_text("You haven't specified which song to look for!")
+        return
+    else:
+        song = Song.find_song(query)
+        if song:
+            if song.lyrics:
+                reply = song.format()
+            else:
+                reply = "Couldn't find any lyrics for that song!"
+        else:
+            reply = "Song not found!"
+        if len(reply) > 4090:
+            with open("lyrics.txt", 'w') as f:
+                f.write(f"{reply}\n\n\nOwO UwU OmO")
+            with open("lyrics.txt", 'rb') as f:
+                msg.reply_document(document=f,
+                caption="Message length exceeded max limit! Sending as a text file.")
+        else:
+            msg.reply_text(reply)
+
+
+LYRICS_HANDLER = CommandHandler("lyrics", lyrics, pass_args=True)
+
 dispatcher.add_handler(LYRICS_HANDLER)
