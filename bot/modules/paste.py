911ff65441b38684b3c7ed852835c81c3f71b7ca
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-11-02 01:53:51 +0700

    Update paste.py

--- bot/modules/paste.py
+++ bot/modules/paste.py
@@ -1,7 +1,7 @@
 import httpx
 
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import Message, User, InlineKeyboardButton, InlineKeyboardMarkup
 from bot import app, dispatcher
 from telegram.ext import CommandHandler
 
@@ -21,7 +21,12 @@ async def hastebin(c: Client, m: Message):
         url = "https://hastebin.com/documents"
         r = await http.post(url, data=mean.encode("UTF-8"))
         url = f"https://hastebin.com/{r.json()['key']}"
-        await m.reply_photo('https://telegra.ph/file/035a463a52268420f6a0c.jpg', caption=url)
+        markup = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⚙ Klik disini ⚙', url=url)
+        ]]
+    )
+        await m.reply_photo('https://telegra.ph/file/035a463a52268420f6a0c.jpg', caption=f"Request paste by {m.from_user.mention}", reply_markup=markup)
     else:
         await m.reply_text("reply_to_document_or_text")
 
