05fe04157f862b7fa484590380247c0e2351276f
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-11-04 05:52:28 +0700

    Update rbg.py

--- bot/modules/rbg.py
+++ bot/modules/rbg.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message
 from bot import app
 
 REMOVE_BG_API = "cvwHMNycC6yQdGJeRZgWLt2Y"
-PATH = "/usr/src/app/downloads/"
+PATH = "./DOWNLOADS/"
 
 
 @app.on_message(filters.command(["removebg"]))
@@ -18,21 +18,38 @@ async def remove_background(client, message):
           )
         return
     await message.reply_text("Analysing...")
+    new_file = PATH + str(update.from_user.id) + "/"
+    new_file_name = new_file + "no_bg."
     replied = message.reply_to_message
     if (replied and replied.media
             and (replied.photo
                  or (replied.document and "image" in replied.document.mime_type))):
-            rmbg = RemoveBg(REMOVE_BG_API)
-            rmbg.remove_background_from_img_file(PATH)
-            rbg_path = PATH + "_no_bg.png"
-            await app.send_document(
-                chat_id=message.chat.id,
-                document=rbg_path,
-                disable_notification=True,
-                )
-            await message.delete()
+        file = await update.download(PATH+str(update.from_user.id))
+        await message.edit_text(
+            text="Photo downloaded successfully. Now removing background.",
+            disable_web_page_preview=True
+        )
+        new_document = removebg_image(file)
+    if new_document.status_code == 200:
+        with open(new_file_name, "wb") as file:
+            file.write(new_document.content)
+            await message.reply_chat_action("upload_document")
+    try:
+        await message.reply_document(document=new_file_name, quote=True)
+        try:
+            os.remove(file)
+        except:
+            pass
         except Exception:  # pylint: disable=broad-except
             await message.reply_text("Something went wrong!\nCheck your usage quota!")
-            return
     else:
         await message.reply_text("Reply to a photo to remove background!")
+
+
+def removebg_image(file):
+    return requests.post(
+        "https://api.remove.bg/v1.0/removebg",
+        files={"image_file": open(file, "rb")},
+        data={"size": "auto"},
+        headers={"X-Api-Key": REMOVEBG_API}
+    )
