7d7213413af082fac36daa7d743d227eda59b634
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-11-04 04:42:52 +0700

    Update qrcode.py

--- bot/modules/qrcode.py
+++ bot/modules/qrcode.py
@@ -3,20 +3,22 @@ import png
 from pyrogram import Client, filters
 from pyqrcode import QRCode
 from bot import app
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+from telegram import Update
 
 
-@app.on_message(filters.command(["mkqr"]))
-def qrcode(client, message):
+@Client.on_message(filters.regex(pattern=".*http.*") & filters.private)
+def qrcode(update: Update, context: CallbackContext):
     text = message.text.split(" ", 1)[1]
-    message_id = message.message_id
+    message_id = update.message.message_id
     qr_file = f'{message_id}.png'
     try:
-        await message.reply_text("Generating")
+        update.message.reply_text("Generating")
         Qr_Code = QRCode(text)
         Qr_Code.png(qr_file, scale=10)
-        await message.reply_photo(photo=open(
+        update.message.reply_photo(photo=open(
             qr_file, "rb"), reply_to_message_id=message_id, caption=f"Here is Your Qr code for '{text}'")
-        await message.reply_text("Finished")
+        update.message.reply_text("Finished")
         os.remove(qr_file)
     except Exception:
-        await message.reply_text("Please Try Agian Later")
+        update.message.reply_text("Please Try Agian Later")
