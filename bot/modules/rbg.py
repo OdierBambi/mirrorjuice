c3868df8f26583381c1d58fd82451288f6625651
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-11-04 02:56:30 +0700

    Update rbg.py

--- bot/modules/rbg.py
+++ bot/modules/rbg.py
@@ -9,7 +9,7 @@ PATH = "./DOWNLOADS/"
 
 
 @app.on_message(filters.command(["removebg"]))
-async def remove_background(message):
+async def remove_background(client, message):
     if not REMOVE_BG_API:
         await update.reply_text(
             text="Error :- Remove BG Api is error",
