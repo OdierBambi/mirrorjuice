afdd73d2767544d885b042b23b74b23836793dc1
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-11-04 12:15:59 +0700

    Add files via upload

---
+++ bot/modules/github.py
@@ -0,0 +1,49 @@
+import os
+from requests import get
+from pyrogram import filters
+from bot import app, dispatcher
+
+
+
+@app.on_message(filters.command(['github']))
+def git(_,message):
+    user = message.text.split(' ')[1]
+    res = get(f'https://api.github.com/users/{user}').json()
+    if res.status_code == 200:
+    status_message = await message.reply_text("`fetching github info ...`")
+        data = res.json()
+        photo = data["avatar_url"]
+        if data['bio']:
+            data['bio'] = data['bio'].strip()
+        repos = []
+        sec_res = requests.get(data["repos_url"])
+        if sec_res.status_code == 200:
+            limit = int(message.flags.get('-l', 5))
+            for repo in sec_res.json():
+                repos.append(f"[{repo['name']}]({repo['html_url']})")
+                limit -= 1
+                if limit == 0:
+                    break
+        template = """
+\b👤 **Name** : [{name}]({html_url})
+🔧 **Type** : `{type}`
+🏢 **Company** : `{company}`
+🔭 **Blog** : {blog}
+📍 **Location** : `{location}`
+📝 **Bio** : __{bio}__
+❤️ **Followers** : `{followers}`
+👁 **Following** : `{following}`
+📊 **Public Repos** : `{public_repos}`
+📄 **Public Gists** : `{public_gists}`
+🔗 **Profile Created** : `{created_at}`
+✏️ **Profile Updated** : `{updated_at}`\n""".format(**data)
+        if repos:
+            template += "🔍 **Some Repos** : " + ' | '.join(repos)
+        await message.reply_photo(chat_id=message.chat.id,
+                                        caption=template,
+                                        photo=photo,
+                                        disable_notification=True)
+        await status_message.delete()
+      else:
+        await status_message.edit("No user found with `{}` username!".format(username))
+    
