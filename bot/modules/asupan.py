9338257c9d62d273da8a76a775523a46afe79fb6
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-11-07 12:59:06 +0700

    Create asupan.py

---
+++ bot/modules/asupan.py
@@ -0,0 +1,18 @@
+import httpx
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from bot import app
+
+@app.on_message(filters.command(["chika"]))
+async def chika(c: Client, m: Message):
+
+     http = httpx.AsyncClient(http2=True)
+     r = await http.get("https://api-tede.herokuapp.com/api/chika")
+     response = r.json()
+
+    await c.send_video(
+    chat_id=message.chat.id,
+    response["url"]
+)
