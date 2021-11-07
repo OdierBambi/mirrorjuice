539392a6eae069a3f3d449800fc15cac34c38908
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-11-07 15:54:09 +0700

    Update wife.py

--- bot/modules/wife.py
+++ bot/modules/wife.py
@@ -19,10 +19,3 @@ async def waifu(c: Client, m: Message):
      r = await http.get("https://api.waifu.pics/nsfw/waifu")
      rj = r.json()
      await m.reply_photo(rj["url"], caption=f"Ini waifumu 🤤 {m.from_user.mention}")
-
-@app.on_message(filters.command(['nwife']))
-async def waifu(c: Client, m: Message):
-     http = httpx.AsyncClient(http2=True)
-     r = await http.get("https://api.waifu.pics/nsfw/waifu")
-     rj = r.json()
-     await m.reply_photo(rj["url"], caption=f"Ini waifumu 🤤 {m.from_user.mention}")
