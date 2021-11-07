f8eaf091aff370d0adcb276206ad275fc5a1a29e
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-11-07 15:53:44 +0700

    Update wife.py

--- bot/modules/wife.py
+++ bot/modules/wife.py
@@ -3,19 +3,26 @@ import httpx
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from bot import app, dispatcher
-from telegram.ext import CommandHandler
+from bot import app
 
 @app.on_message(filters.command(['wife']))
 async def waifu(c: Client, m: Message):
-
      http = httpx.AsyncClient(http2=True)
      r = await http.get("https://api.waifu.pics/sfw/waifu")
      rj = r.json()
-
      await m.reply_photo(rj["url"], caption=f"Ini waifumu 😉 {m.from_user.mention}")
 
 
-WIFE_HANDLER = CommandHandler("wife", waifu)
+@app.on_message(filters.command(['nwife']))
+async def waifu(c: Client, m: Message):
+     http = httpx.AsyncClient(http2=True)
+     r = await http.get("https://api.waifu.pics/nsfw/waifu")
+     rj = r.json()
+     await m.reply_photo(rj["url"], caption=f"Ini waifumu 🤤 {m.from_user.mention}")
 
-dispatcher.add_handler(WIFE_HANDLER)
+@app.on_message(filters.command(['nwife']))
+async def waifu(c: Client, m: Message):
+     http = httpx.AsyncClient(http2=True)
+     r = await http.get("https://api.waifu.pics/nsfw/waifu")
+     rj = r.json()
+     await m.reply_photo(rj["url"], caption=f"Ini waifumu 🤤 {m.from_user.mention}")
