848086c41cfd7cfdffe5a0f6a3f143fee8f51ab1
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-11-09 03:43:42 +0700

    Create info.py

---
+++ bot/modules/info.py
@@ -0,0 +1,94 @@
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/Info-Bot/blob/main/LICENSE
+
+import os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from bot import app
+
+
+
+START_TEXT = """
+Hello {}, I am a user or chat information finder telegram bot.
+
+- Send /info for your info
+- Send /info reply to a forward message for chat or user info
+
+Made by @FayasNoushad
+"""
+BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')
+        ]]
+    )
+
+@app.on_message(filters.command(["hlp"]))
+async def start(bot, update):
+    text = START_TEXT.format(update.from_user.mention)
+    reply_markup = BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup,
+        quote=True
+    )
+
+@app.on_message(filters.command(["info", "information"]))
+async def info(bot, update):
+    if (not update.reply_to_message) and ((not update.forward_from) or (not update.forward_from_chat)):
+        info = user_info(update.from_user)
+    elif update.reply_to_message and update.reply_to_message.forward_from:
+        info = user_info(update.reply_to_message.forward_from)
+    elif update.reply_to_message and update.reply_to_message.forward_from_chat:
+        info = chat_info(update.reply_to_message.forward_from_chat)
+    elif (update.reply_to_message and update.reply_to_message.from_user) and (not update.forward_from or not update.forward_from_chat):
+        info = user_info(update.reply_to_message.from_user)
+    else:
+        return
+    try:
+        await update.reply_text(
+            text=info,
+            reply_markup=BUTTONS,
+            disable_web_page_preview=True,
+            quote=True
+        )
+    except Exception as error:
+        await update.reply_text(error)
+
+def user_info(user):
+    text = "--**User Details:**--\n"
+    text += f"\n**First Name:** `{user.first_name}`"
+    text += f"\n**Last Name:** `{user.last_name},`" if user.last_name else ""
+    text += f"\n**User Id:** `{user.id}`"
+    text += f"\n**Username:** @{user.username}" if user.username else ""
+    text += f"\n**User Link:** {user.mention}" if user.username else ""
+    text += f"\n**DC ID:** `{user.dc_id}`" if user.dc_id else ""
+    text += f"\n**Is Deleted:** True" if user.is_deleted else ""
+    text += f"\n**Is Bot:** True" if user.is_bot else ""
+    text += f"\n**Is Verified:** True" if user.is_verified else ""
+    text += f"\n**Is Restricted:** True" if user.is_verified else ""
+    text += f"\n**Is Scam:** True" if user.is_scam else ""
+    text += f"\n**Is Fake:** True" if user.is_fake else ""
+    text += f"\n**Is Support:** True" if user.is_support else ""
+    text += f"\n**Language Code:** {user.language_code}" if user.language_code else ""
+    text += f"\n**Status:** {user.status}" if user.status else ""
+    text += f"\n\nMade by @FayasNoushad"
+    return text
+
+def chat_info(chat):
+    text = "--**Chat Details**--\n" 
+    text += f"\n**Title:** `{chat.title}`"
+    text += f"\n**Chat ID:** `{chat.id}`"
+    text += f"\n**Username:** @{chat.username}" if chat.username else ""
+    text += f"\n**Type:** `{chat.type}`"
+    text += f"\n**DC ID:** `{chat.dc_id}`"
+    text += f"\n**Is Verified:** True" if chat.is_verified else ""
+    text += f"\n**Is Restricted:** True" if chat.is_verified else ""
+    text += f"\n**Is Creator:** True" if chat.is_creator else ""
+    text += f"\n**Is Scam:** True" if chat.is_scam else ""
+    text += f"\n**Is Fake:** True" if chat.is_fake else ""
+    text += f"\n\nMade by @FayasNoushad"
+    return text
